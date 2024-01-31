a7fb4ec84f9d6d95f018039369ebf39f69ce76b1
svhl <145035644+svhl@users.noreply.github.com>
2024-01-31 22:39:17 +0530

    improved vpn script for kde

--- vpn.sh
+++ vpn.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
 if [[ $(protonvpn-cli s) == "
-No active Proton VPN connection." ]]
-then
-	protonvpn-cli c -f
+No active Proton VPN connection." ]]; then
+	kdialog --title "VPN" --yesno "VPN appears to be disconnected. Do you want to connect?" --yes-label "Connect" --no-label "Disconnect"
+	
+	if [ $? == 0 ]; then
+		# disconnect in case vpn not properly disconnected before
+		# connected but no network activity
+		protonvpn-cli d
+		sleep 1
+		protonvpn-cli c -f
+
+	else
+		protonvpn-cli d
+	fi
+
 else
-	protonvpn-cli d
+	kdialog --title "VPN" --yesno "VPN appears to be connected. Do you want to disconnect?" --yes-label "Disconnect" --no-label "Connect"
+	
+	if [ $? == 0 ]; then
+		protonvpn-cli d
+
+	else
+		protonvpn-cli d
+		sleep 1
+		protonvpn-cli c -f
+	fi
 fi
