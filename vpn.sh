6b3f9462a3acdc0c126860219c784e202caedf83
svhl <145035644+svhl@users.noreply.github.com>
2024-02-01 11:43:54 +0530

    improved vpn script for zenity/yad

--- vpn.sh
+++ vpn.sh
@@ -2,7 +2,15 @@
 
 if [[ $(protonvpn-cli s) == "
 No active Proton VPN connection." ]]; then
-	kdialog --title "VPN" --yesno "VPN appears to be disconnected. Do you want to connect?" --yes-label "Connect" --no-label "Disconnect"
+	if [[ $(dpkg -l | grep kdialog) != '' ]]; then
+		kdialog --title "VPN" --yesno "VPN appears to be disconnected. Do you want to connect?" --yes-label "Connect" --no-label "Disconnect"
+
+	elif [[ $(dpkg -l | grep zenity) != '' ]]; then
+		zenity --question --title=VPN --text="VPN appears to be disconnected. Do you want to connect?" --ok-label "Connect" --cancel-label "Disconnect"
+
+	elif [[ $(dpkg -l | grep yad) != '' ]]; then
+		yad --image dialog-question --title VPN --button "Connect" --button "Disconnect" --text "VPN appears to be disconnected. Do you want to connect?"
+	fi
 	
 	if [ $? == 0 ]; then
 		# disconnect in case vpn not properly disconnected before
@@ -16,8 +24,16 @@ No active Proton VPN connection." ]]; then
 	fi
 
 else
-	kdialog --title "VPN" --yesno "VPN appears to be connected. Do you want to disconnect?" --yes-label "Disconnect" --no-label "Connect"
-	
+	if [[ $(dpkg -l | grep kdialog) != '' ]]; then
+		kdialog --title "VPN" --yesno "VPN appears to be connected. Do you want to disconnect?" --yes-label "Disconnect" --no-label "Connect"
+
+	elif [[ $(dpkg -l | grep zenity) != '' ]]; then
+		zenity --question --title=VPN --text="VPN appears to be connected. Do you want to disconnect?" --ok-label "Disconnect" --cancel-label "Connect"
+
+	elif [[ $(dpkg -l | grep yad) != '' ]]; then
+		yad --image dialog-question --title VPN --button "Disconnect" --button "Connect" --text "VPN appears to be connected. Do you want to disconnect?"
+	fi
+
 	if [ $? == 0 ]; then
 		protonvpn-cli d
 
